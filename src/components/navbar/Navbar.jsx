3b141feec5362ffe5fed2456b8c74f237f1273e1
sergioneves-grupoma <sergio.neves@madigital.eu>
2024-05-24 18:09:33 +0100

    Added Navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import "./navbar.scss";
+function Navbar() {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png"></img>
+          <span>Brand Name</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contacts</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png"></img>
+        </div>
+        <div className="menu">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
